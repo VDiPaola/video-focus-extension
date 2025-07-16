e719b4b21c0366381a7190618b68235efe0e290e
Enzo <dipaolae98@gmail.com>
2025-07-16 20:04:46 +0100

    clean up

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,7 @@ const path = require('path');
 module.exports = {
     entry: {
         'content-scripts/main': './src/content-scripts/main.js',
-        'background': './src/background-scripts/main.js',
         'popup/popup': './popup/popup.js',
-        //'options-page/main': './src/options-page/main.js',
-        
-        
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
